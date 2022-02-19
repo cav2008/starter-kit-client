afaf711ca6fcd61dd18dc7ff287cdbe04d4ff191
Cavan Law <cavan.law@viooh.com>
2022-02-19 12:14:23 +0000

    add initial babel and eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const containerDOM = document.getElementById('container');
+const containerDOM = document.getElementById("container");
 
 ReactDOM.render(<App />, containerDOM);
