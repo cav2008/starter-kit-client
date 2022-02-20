37e97e4b1dfffa88129d990d7b2093f89db8772f
Cavan Law <cavan.law@viooh.com>
2022-02-20 13:10:45 +0000

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './global.pcss';
+
 const App = () => {
   console.log('Hello World');
 
-  return <h1>Hello, world!</h1>;
+  return <h1 className="text-8xl text-blue-600">Hello, world!</h1>;
 };
 
 export default App;
