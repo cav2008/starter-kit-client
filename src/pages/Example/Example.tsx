bed8231cdef0a3a6b36a262485a7e5b80e675c24
Cavan Law <cavan.law@viooh.com>
2022-04-03 16:35:39 +0100

    give css module styles better name

--- src/pages/Example/Example.tsx
+++ src/pages/Example/Example.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import styles from './Example.module.pcss';
 
-const Example = () => <h1 className={styles.examplePink}>Example</h1>;
+const Example = () => <h1 className={styles.title}>Example</h1>;
 
 export default Example;
