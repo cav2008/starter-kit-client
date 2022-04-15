fb0812f5b974dc797e7422df1d33bcc8eebb9f0a
Cavan Law <cavan.law@viooh.com>
2022-04-15 12:35:00 +0100

    update reactdom render with new react 18 way

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import store from './store';
@@ -7,9 +7,9 @@ import App from './App';
 
 const containerDOM = document.getElementById('container');
 
-ReactDOM.render(
+const root = createRoot(containerDOM!);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  containerDOM
+  </Provider>
 );
